0979d1e5ab2a1dc4b5962ac76be6143766f7ed83
Max Ogden <max@maxogden.com>
2014-11-21 22:45:22 +0800

    key events

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 window.debug = require('debug')
+var synthEvent = window.synthEvent = require('synthetic-dom-events')
 var SimplePeer = require('simple-peer')
 var url = require('url')
 var zlib = require('zlib')
 var video, remoteWidth, remoteHeight
 
 var fakeMouse = document.createElement('div')
-fakeMouse.setAttribute('style', 'width: 5px; height: 5px; background: salmon; position: absolute;')
+fakeMouse.style.width = '5px'
+fakeMouse.style.height = '5px'
+fakeMouse.style.background = 'salmon'
+fakeMouse.style.position = 'absolute'
+fakeMouse.style.pointerEvents = 'none'
+fakeMouse.style.transition = 'top 0.2s, left 0.2s ease-out'
+
 
 var qs = url.parse(window.location.href, true).query
 var constraints = {
@@ -31,6 +38,9 @@ if (!qs.remote) {
       console.error('PERMISSION_DENIED. Are you on SSL? Have you enabled the --enable-usermedia-screen-capturing flag?')
     }
   })
+  
+
+  
 } else {
   var peer = new SimplePeer({ trickle: false })
   handleSignal(peer)
@@ -65,20 +75,37 @@ function handleSignal(peer) {
     console.log(JSON.stringify(data), (Date.now() - last) + 'ms')
     last = Date.now()
     
-    if (!data.canvasWidth) return;
-    projectedX = data.clientX / data.canvasWidth * screen.width;
-    projectedY = data.clientY / data.canvasHeight * screen.height;
+    if (!data.canvasWidth) return
+    projectedX = data.clientX / data.canvasWidth * screen.width
+    projectedY = data.clientY / data.canvasHeight * screen.height
     
-    realScreenX = lastData.screenX - lastData.clientX;
-    realScreenY = lastData.screenY - lastData.clientY;
+    realScreenX = lastData.screenX - lastData.clientX
+    realScreenY = lastData.screenY - lastData.clientY
     
-    pointX = projectedX - realScreenX | 0;
+    pointX = projectedX - realScreenX | 0
     pointY = projectedY - realScreenY | 0
-    
-    console.log('projectedX', projectedX, 'realScreenX', realScreenX, 'pointX', pointX);
     
     fakeMouse.style.top = pointY + 'px'
     fakeMouse.style.left = pointX + 'px'
+    
+    if (data.click) {
+      var targetEl = document.elementFromPoint(pointX, pointY)
+      console.log(targetEl)
+      var clickOpts = {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      }
+      
+      targetEl.dispatchEvent(synthEvent('click', clickOpts))
+    }
+    
+    if (data.keydown && data.keydown.length) {
+      data.keydown.forEach(function(e) {
+        var el = document.activeElement
+        el.dispatchEvent(synthEvent('keydown', e))  
+      })
+    }
   })
   
   if (!qs.remote) document.body.appendChild(fakeMouse)
@@ -90,31 +117,46 @@ function handleSignal(peer) {
           if (!needsSend) return console.log('does not need send')
           peer.send(lastData)
           needsSend = false
-        }, 250)
+          lastData = {}
+        }, 100)
       })
     }
     
     video = document.createElement('video')
     video.src = window.URL.createObjectURL(stream)
     video.autoplay = true
-    document.body.appendChild(video)
+    var container = document.querySelector('.container')
+    container.appendChild(video)
   })
 }
 
-var lastData = 0, needsSend = true
+var lastData = {}, needsSend = true
+window.addEventListener('mousedown', function(e){
+  lastData.click = true
+  updateLastData(e)
+})
+
 window.addEventListener('mousemove', function(e){
-  lastData = {
-    screenX: e.screenX,
-    screenY: e.screenY,
-    clientX: e.clientX,
-    clientY: e.clientY
-  }
+  updateLastData(e)
+})
+
+window.addEventListener('keydown', function(e) {
+  lastData.keydown = lastData.keydown || []
+  lastData.keydown.push({keyCode: e.keyCode})
+  needsSend = true
+})
+
+function updateLastData(e) {
+  lastData.screenX = e.screenX
+  lastData.screenY = e.screenY
+  lastData.clientX = e.clientX
+  lastData.clientY = e.clientY
   
   if (video) {
-    var rects = video.getBoundingClientRect();
-    lastData.canvasWidth = rects.width;
-    lastData.canvasHeight = rects.height;
+    var rects = video.getBoundingClientRect()
+    lastData.canvasWidth = rects.width
+    lastData.canvasHeight = rects.height
   }
 
   needsSend = true
-})
+}
