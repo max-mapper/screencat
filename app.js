71e5ea3c2f5208817160d8778f02980ff8270be9
Max Ogden <max@maxogden.com>
2015-03-27 14:15:12 -0700

    update links

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var ipc = require('ipc')
 var mb = menubar({
   width: 700,
   height: 300,
-  index: 'file://' + path.join(process.cwd(), 'app.html')
+  index: 'file://' + path.join(process.cwd(), 'app.html'),
+  icon: 'file://' + path.join(process.cwd(), 'img', 'Icon.png')
 })
 
 mb.on('ready', function ready () {
