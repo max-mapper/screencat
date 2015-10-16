32b37c66e85f4d22457e545759e4dec8a21ad229
Max Ogden <max@maxogden.com>
2015-10-16 15:40:41 -0700

    rename some files

--- app.js
+++ app.js
@@ -1,22 +1,98 @@
-var path = require('path')
-var menubar = require('menubar')
 var ipc = require('ipc')
+var clipboard = require('clipboard')
+var shell = require('shell')
 
-var mb = menubar({
-  width: 700,
-  height: 300,
-  index: 'file://' + path.join(__dirname, 'app.html'),
-  icon: 'file://' + path.join(__dirname, 'img', 'Icon.png')
+var createPeerConnection = require('./peer.js')
+var ui = require('./ui.js')
+var connect = require('./connect.js')
+
+var peer
+var peerConnection = createPeerConnection()
+
+peerConnection.on('connected', function connected (newPeer, remote) {
+  peer = newPeer
+
+  if (!remote) {
+    ui.show(ui.containers.sharing)
+    ui.hide(ui.containers.content)
+  } else {
+    ipc.send('resize', {width: 800, height: 500})
+    ui.show(ui.containers.multimedia)
+    ui.hide(ui.containers.content)
+  }
+
+  peer.on('error', function error (err) {
+    console.error('peer error')
+    console.error(err)
+    ui.containers.content.innerHTML = 'Error connecting! Please Quit. ' + err.message
+  })
+
+  peer.on('close', function close () {
+    showChoose()
+  })
 })
 
-mb.on('ready', function ready () {
-  console.log('ready')
+ui.buttons.quit.addEventListener('click', function (e) {
+  ipc.send('terminate')
 })
 
-ipc.on('terminate', function terminate (ev) {
-  mb.app.terminate()
+ui.buttons.destroy.addEventListener('click', function (e) {
+  if (peer) peer.destroy()
+  showChoose()
 })
 
-ipc.on('resize', function resize (ev, data) {
-  mb.window.setSize(data.width, data.height)
+ui.buttons.share.addEventListener('click', function (e) {
+  ui.show(ui.containers.share)
+  ui.hide(ui.containers.choose)
+  ui.show(ui.buttons.back)
+  if (!peerConnection.robot) peerConnection.robot = require('./robot.js')
+  connect.host(peerConnection, ui)
 })
+
+ui.buttons.join.addEventListener('click', function (e) {
+  ui.show(ui.containers.join)
+  ui.hide(ui.containers.choose)
+  ui.show(ui.buttons.back)
+  connect.verifyUserRoom(peerConnection, ui, function (err, room, config) {
+    if (err) {
+      ui.inputs.paste.value = 'Error! ' + err.message
+      return
+    }
+    ui.inputs.paste.value = 'Waiting on other side...'
+    connect.remote(peerConnection, ui, room, config)
+  })
+})
+
+ui.buttons.back.addEventListener('click', function (e) {
+  // HACK do a clone-swap to remove listeners
+  var el = ui.buttons.paste
+  var elClone = el.cloneNode(true)
+  el.parentNode.replaceChild(elClone, el)
+  ui.buttons.paste = elClone
+
+  showChoose()
+})
+
+ui.buttons.copy.addEventListener('click', function (e) {
+  e.preventDefault()
+  clipboard.writeText(ui.inputs.copy.value)
+})
+
+function showChoose () {
+  ui.hide(ui.containers.sharing)
+  ui.hide(ui.containers.multimedia)
+  ui.show(ui.containers.content)
+  ui.show(ui.containers.choose)
+  ui.hide(ui.containers.share)
+  ui.hide(ui.containers.join)
+  ui.hide(ui.buttons.back)
+}
+
+var externalLinks = document.querySelectorAll('.open-externally')
+for (var i = 0; i < externalLinks.length; i++) {
+  externalLinks[i].onclick = function (e) {
+    e.preventDefault()
+    shell.openExternal(e.target.href)
+    return false
+  }
+}
---
+++ electron.js
@@ -0,0 +1,22 @@
+var path = require('path')
+var menubar = require('menubar')
+var ipc = require('ipc')
+
+var mb = menubar({
+  width: 700,
+  height: 300,
+  index: 'file://' + path.join(__dirname, 'app.html'),
+  icon: 'file://' + path.join(__dirname, 'img', 'Icon.png')
+})
+
+mb.on('ready', function ready () {
+  console.log('ready')
+})
+
+ipc.on('terminate', function terminate (ev) {
+  mb.app.terminate()
+})
+
+ipc.on('resize', function resize (ev, data) {
+  mb.window.setSize(data.width, data.height)
+})
